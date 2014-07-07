141c81ed21bbc58d4bef0d06330791f3991eea1a
Stanislav Yashenkov <vampire@realtymag.ru>
2014-07-07 12:36:55 +0600

    benchmark adopted

--- support/benchmark.js
+++ support/benchmark.js
@@ -1,34 +1,32 @@
+/*jshint multistr: true */
 
-/**
- * Module dependencies.
- */
-
-var uubench = require('uubench')
-  , jade = require('../');
-
+var uubench = require('uubench');
+var jade = require('../');
 
 var suite = new uubench.Suite({
-  min: 200,
-  result: function(name, stats){
-    var persec = 1000 / stats.elapsed
-      , ops = stats.iterations * persec;
-    console.log('%s: %d', name, ops | 0);
-  }
+    type: "fixed",
+    iterations: 10000,
+    result: function (name, stats) {
+        var persec = 1000 / stats.elapsed;
+        var ops = stats.iterations * persec;
+        console.log('%s: %d', name, Math.floor(ops));
+    }
 });
 
 function setup(self) {
-  var suffix = self ? ' (self)' : ''
-    , options = { self: self };
+    var suffix = self ? ' (self)' : '';
+    var options = {
+        self: self
+    };
 
-  var str = 'html\n  body\n    h1 Title'
-    , fn = jade.compile(str, options);
+    var str = 'html\n  body\n    h1 Title';
+    var fn1 = jade.compile(str, options);
 
-  suite.bench('tiny' + suffix, function(next){
-    fn();
-    next();
-  });
+    suite.bench('tiny' + suffix, function (next) {
+        fn1().done(next);
+    });
 
-str = '\
+    str = '\
 html\n\
   body\n\
     h1 Title\n\
@@ -37,44 +35,48 @@ html\n\
       li: a(href="#") About Us\n\
       li: a(href="#") Store\n\
       li: a(href="#") FAQ\n\
-      li: a(href="#") Contact\n\
-';
+      li: a(href="#") Contact\n';
 
-  var fn2 = jade.compile(str, options);
+    var fn2 = jade.compile(str, options);
 
-  suite.bench('small' + suffix, function(next){
-    fn2();
-    next();
-  });
+    suite.bench('small' + suffix, function (next) {
+        fn2().done(next);
+    });
 
-str = '\
+    str = '\
 html\n\
   body\n\
     h1 #{title}\n\
     ul#menu\n\
       - each link in links\r\n\
-        li: a(href="#")= link\r\n\
-';
+        li: a(href="#")= link\r\n';
 
-  if (self) {
-str = '\
+    if (self) {
+        str = '\
 html\n\
   body\n\
     h1 #{self.title}\n\
     ul#menu\n\
       - each link in self.links\r\n\
-        li: a(href="#")= link\r\n\
-';
-  }
+        li: a(href="#")= link\r\n';
+    }
 
-  var fn3 = jade.compile(str, options);
+    var fn3 = jade.compile(str, options);
 
-  suite.bench('small locals' + suffix, function(next){
-    fn3({ title: 'Title', links: ['Home', 'About Us', 'Store', 'FAQ', 'Contact'] });
-    next();
-  });
+    suite.bench('small locals' + suffix, function (next) {
+        fn3({
+            title: 'Title',
+            links: [
+                'Home',
+                'About Us',
+                'Store',
+                'FAQ',
+                'Contact'
+            ]
+        }).done(next);
+    });
 
-str = '\
+    str = '\
 html\n\
   body\n\
     h1 Title\n\
@@ -83,18 +85,16 @@ html\n\
       li: a(href="#") About Us\n\
       li: a(href="#") Store\n\
       li: a(href="#") FAQ\n\
-      li: a(href="#") Contact\n\
-';
+      li: a(href="#") Contact\n';
 
-  str = Array(30).join(str);
-  var fn4 = jade.compile(str, options);
+    str = new Array(30).join(str);
+    var fn4 = jade.compile(str, options);
 
-  suite.bench('medium' + suffix, function(next){
-    fn4();
-    next();
-  });
+    suite.bench('medium' + suffix, function (next) {
+        fn4().done(next);
+    });
 
-str = '\
+    str = '\
 html\n\
   body\n\
     h1 Title\n\
@@ -103,16 +103,14 @@ html\n\
       li: a(href="#") About Us\n\
       li: a(href="#") Store\n\
       li: a(href="#") FAQ\n\
-      li: a(href="#") Contact\n\
-';
+      li: a(href="#") Contact\n';
 
-  str = Array(100).join(str);
-  var fn5 = jade.compile(str, options);
+    str = new Array(100).join(str);
+    var fn5 = jade.compile(str, options);
 
-  suite.bench('large' + suffix, function(next){
-    fn5();
-    next();
-  });
+    suite.bench('large' + suffix, function (next) {
+        fn5().done(next);
+    });
 }
 
 setup();
